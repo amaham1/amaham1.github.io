5cebf3347e4099aae782f341347bbada37da2240
최주영 <akapwhd@gmail.com>
2025-02-04 19:47:49 +0900

    grap 폽ㄱ사

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import RealEstateList from '@/components/RealEstate/RealEstateList.vue';
+import RealEstateDetail from '@/components/RealEstate/RealEstateDetail.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'RealEstateList',
+    component: RealEstateList
+  },
+  {
+    path: '/property/:id',
+    name: 'RealEstateDetail',
+    component: RealEstateDetail,
+    props: route => ({ propertyId: Number(route.params.id) })
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
